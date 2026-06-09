51878ba8fe4fadb649508054222a34b4c06239fe
Andrew Moy <am5030@georgetown.edu>
2026-06-09 16:17:21 -0400

    full pipeline skeleton

--- repodoc/cli.py
+++ repodoc/cli.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 from repodoc.discover import discover
+from repodoc.state import remove_unchanged_repos
 from repodoc.scan import scan
 import argparse
 
@@ -27,13 +28,25 @@ def main():
     parser = create_cli_interface()
     args = parser.parse_args()
 
+    # discover repos from local and github using CLI arguments
     log.info("Starting repository discovery with local patterns: %s and GitHub token: %s", args.local, "provided" if args.token else "not provided")
     repos = discover(local_patterns=args.local, github_token=args.token)
-    for repo in repos:
-        print(repo)
+    
+    # delete unchanged repositories
+    log.info("Removing unchanged repositories from the list of discovered repositories")
+    repos = remove_unchanged_repos(repos)
 
+    # scan the discovered repositories to gather information deterministically
+    log.info("Scanning repositories for information")
     scan(repos)
 
+    # passing information to LLM to generate documentation
+    # log.info("Generating documentation for repositories using LLM")
+    # gen_docs(repos)
+
+    # writing documentation to file system
+    # log.info("Writing generated documentation to file system")
+    # write(repos)
 
 if __name__ == "__main__":
     main()
